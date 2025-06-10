310b90f24c83d614dddafc1256336c714379321d
Kelvin <kelvin.le.tech@gmail.com>
2025-06-10 08:35:33 +0700

    191. Number of 1 Bits

--- Blind-75/number_of_1_bits.go
+++ Blind-75/number_of_1_bits.go
@@ -13,6 +13,13 @@ package blind_75
  * - The idea is to repeatedly check the least significant bit and right shift the integer until it becomes zero.
  * Time Complexity: O(log n) - iterating through the bits of the integer once (because the number of bits in an integer is log(n) where n is the value of the integer).
  * Space Complexity: O(1) - constant extra space
+ * Steps:
+ * 1. Initialize a count variable to zero.
+ * 2. While the integer is not zero:
+ *    - Check if the least significant bit is 1 (using bitwise AND with 1).
+ *    - If it is, increment the count.
+ *    - Right shift the integer by one position to check the next bit.
+ * 3. Return the count.
  */
 
 func hammingWeight(n int) int {
